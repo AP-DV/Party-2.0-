00313d3ce941bb79f1852fe524927f10c382416f
Emmanuel <plemel61p2@free.fr>
2026-03-23 11:46:53 +0100

    delete env.test

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 // export const BACKENDADRESS = "http://192.168.1.34:3000";
-export const BACKENDADRESS = "http://192.168.1.160:3000"
+// export const BACKENDADRESS = "http://192.168.1.160:3000"
 // export const BACKENDADRESS = "http://192.168.43.51:3000"
-// export const BACKENDADRESS = "https://party-backend-delta.vercel.app"
+export const BACKENDADRESS = "https://party-backend-delta.vercel.app"
